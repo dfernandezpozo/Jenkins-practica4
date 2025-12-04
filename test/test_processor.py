046403efce0692f94d15307c3489088d31c71ed7
dfernandezpozo <dfernandezpozo@iessonferrer.net>
2025-12-04 17:32:35 +0100

    Add unit test for process_data function

---
+++ test/test_processor.py
@@ -0,0 +1,11 @@
+import unittest
+from data_processor import process_data
+
+class TestProcessor(unittest.TestCase):
+    def test_process_data(self):
+        data = {"age": [20, 25, 30, 16]}
+        result = process_data(data)
+        self.assertEqual(result.loc["mean", "age"], 22.75)
+
+if __name__ == '__main__':
+    unittest.main()
